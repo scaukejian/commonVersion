c2cdd8369d94b0d2900060912a601952246073e5
scaukejian <954657344@qq.com>
2018-03-01 16:47:53 +0800

    first commit

---
+++ .svn/pristine/16/16121ba439646ce7df70d0b0b122496a4b0e9bdf.svn-base
@@ -0,0 +1,8 @@
+//命令空间的定义
+window.edate = {};
+window.edate.ui = {};
+window.edate.ui.grid = {};
+window.edate.ui.page = {};
+window.edate.ui.msgBox = {};
+window.edate.util = {};
+window.edate.report = {};
---
+++ target/sunInSky/js/common/edateInit.js
@@ -0,0 +1,10 @@
+// 短变量与常量的定义
+var ui = window.edate.ui;
+var grid = ui.grid;
+var page = ui.page;
+var util = window.edate.util;
+var msgBox = ui.msgBox;
+page.pageParams = {page : 1, pageSize : 10};
+// 全局变量
+var pageLoad = null;
+var edateToken = {};
